901833e3c9d919687466837085d0d2ac0bf2bbf9
Eugene Sharygin <eush77@gmail.com>
2015-05-30 01:27:49 +0300

    Add --verbose

--- cli.js
+++ cli.js
@@ -3,23 +3,47 @@
 
 var replHere = require('./');
 
+var helpVersion = require('help-version')(usage()),
+    minimist = require('minimist');
+
 var Repl = require('repl');
 
 
-var usage = function (code) {
-  console.log('Usage:  repl-here');
-  process.exit(code || 0);
+function usage() {
+  return 'Usage:  repl-here [-v | --verbose]';
+}
+
+
+var opts = minimist(process.argv.slice(2), {
+  boolean: 'verbose',
+  alias: { v: 'verbose' },
+  unknown: function () {
+    helpVersion.help(1);
+  }
+});
+
+
+var printNameTable = function (nameTable) {
+  Object.keys(nameTable).forEach(function (module) {
+    console.log('%s -> %s', module, nameTable[module]);
+  });
 };
 
 
-var main = function () {
+(function main() {
   var repl = Repl.start('> ');
+  var nameTable = {};
   replHere(repl, process.cwd())
+    .on('load', function (module, name) {
+      nameTable[module] = name;
+    })
     .on('fail', function (err, module) {
       console.error('\rModule failed to load: ' + module);
     })
-    .on('end', repl.displayPrompt.bind(repl));
-};
-
-
-(process.argv.slice(2) == '--help') ? usage() : main();
+    .on('end', function () {
+      if (opts.verbose) {
+        printNameTable(nameTable);
+      }
+      repl.displayPrompt();
+    });
+}());
